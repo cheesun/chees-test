c7658090c7da03721dd8941d074b1333c32d50ee
Cheesun Choong <thecheesun@gmail.com>
2012-07-04 23:48:26 +1000

    change icon for edit 'cancel' and 'delete' also update domplings to allow title of node to be specified

--- js/chees/dompling.js
+++ js/chees/dompling.js
@@ -1,9 +1,11 @@
 /* simple templating
-define the html in your document, generally you want them to be hidden
-instances of "!variable!" int the dom are replaced with unescaped HTML
-instances of "$variable$" in the dom are replaced with the value passed in via a dict {'variable':'new value'}
-instances of %text% in the dom are replaced with that text <-- we use this to escape things like img tags so they dont get loaded when the page loads
-*/
+ * define the html in your document, generally you want them to be hidden
+ * the title of each node, if specified, becomes the instance attribute name and the title attribute is removed
+ * if you would like a title to be set on the output node, append that title as [title]
+ * instances of "!variable!" int the dom are replaced with unescaped HTML
+ * instances of "$variable$" in the dom are replaced with the value passed in via a dict {'variable':'new value'}
+ * instances of %text% in the dom are replaced with that text <-- we use this to escape things like img tags so they dont get loaded when the page loads
+ */
 
 
 goog.provide('chees.Dompling');  
@@ -17,20 +19,6 @@ chees.Dompling = function(rootNode) {
     this.html = this.rootNode.innerHTML;
     this.rootNode.style.display = "none";
 }
-
-/*chees.Dompling = function(target) {
-    var self = this;
-    function load () {
-        self.html = this.getResponseText();
-        var tmp = goog.dom.createDom('div');
-        tmp.innerHTML = self.html;
-        self.rootNode = tmp.firstChild;
-    }
-    
-    this.rootNode = null;
-    this.html = null;
-    goog.net.XhrLite.send(target,load);
-}*/
 
 chees.Dompling.trim = function (s) {
     return s.replace(/^\s*/, "").replace(/\s*$/, "");
@@ -65,8 +53,16 @@ chees.Dompling.prototype.steam = function (root,data) {
     var output = { root : newdom };
     for (var i = 0; i < elements.length; i++) {
         if (goog.object.containsKey(elements[i],"title")) {
-            output[elements[i].getAttribute("title")] = elements[i];
-            elements[i].removeAttribute("title");
+            var node = elements[i];
+            var title = node.getAttribute("title");
+            var groups = /(.*)\[(.*)\]/.exec(title);
+            if (groups === null) {
+                output[title] = node;
+                node.removeAttribute("title");    
+            } else {
+                output[groups[1]] = node;
+                node.setAttribute("title",groups[2]);
+            }
         }
     }
     
